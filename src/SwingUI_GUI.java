37f1f95a795f05bb147eee300958c3000797ac39
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2023-05-09 17:31:47 -0400

    creating new gui

---
+++ src/SwingUI_GUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class SwingUI_GUI {
+    private JPanel panel1;
+    private JScrollPane MainListAreaScroll;
+    private JScrollPane SideBarScroll;
+    private JButton SearchBTN;
+    private JButton DeleteBTN;
+    private JButton UpdateBTN;
+    private JButton CreateBTN;
+}
